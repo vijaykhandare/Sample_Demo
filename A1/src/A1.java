fc0eac9bfcf6a49738c3eaf34dcfff4f6d74e5c0
vijay <khandarevijay12@gmail.com>
2019-03-25 14:45:30 +0530

    simple commited

---
+++ A1/src/A1.java
@@ -0,0 +1,126 @@
+class Q
+{
+int num;
+boolean valueset=false;
+
+
+	public synchronized void put(int  num) 
+	
+	{	
+		while(valueset)
+		{
+			
+			try
+			{
+				
+				wait();
+			}catch(Exception e)
+			{
+				System.out.println(e);
+			}
+			
+		}
+		System.out.println("put :- "+num);
+		this.num=num;
+		valueset=true;
+		notify();
+		
+		
+		
+	}
+	
+	
+	public synchronized void get()
+	{
+
+		
+		while(!valueset)
+		{
+			
+			try
+			{
+				
+				wait();
+			}catch(Exception e)
+			{
+				System.out.println(e);
+			}
+			
+		}
+		System.out.println("get :- "+num);
+		valueset=false;
+		notify();
+	}
+}
+
+
+class produce implements Runnable
+{
+	Q q;
+	
+	produce( Q q)
+	{
+		this.q=q;
+		
+	
+	Thread t=new Thread(this,"produce");
+	t.start();
+	}
+	public void run()
+	{int i=0;
+		while(true)
+		{
+			q.put(i++);
+			try{
+			Thread.sleep(1000);}
+			catch(Exception w)
+			{
+				System.out.println();
+			}
+		}
+	}
+	
+	
+}
+
+class consume implements Runnable
+{
+
+	Q q;
+	consume(Q q)
+	{
+		this.q=q;
+
+		Thread t=new Thread(this,"consume");
+		t.start();
+	}
+	
+	
+	public void run()
+	{
+	
+		
+		while(true)
+		{
+			q.get();
+			try{
+			Thread.sleep(10000);
+			}catch(Exception w)
+			{
+				
+			}
+			
+		}
+	}
+}
+public class A1 
+{
+	public static void main(String[] args) {
+		Q q=new Q();
+		new produce(q);
+		new consume(q);
+	}
+	
+}
+
+
