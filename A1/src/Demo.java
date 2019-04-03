391714e1895dd6ca1657a64ba33cb1b6433a72d7
vijay <khandarevijay12@gmail.com>
2019-04-03 17:31:01 +0530

    ddsd

--- A1/src/Demo.java
+++ A1/src/Demo.java
@@ -3,10 +3,11 @@ public class Demo {
 
 	
 	public static void main(String[] args) {
-		String name[]={"vijay","sanjay","ajay"};
+		String name[]={"vijay","sanjay","ajay","billa"}; // this is string
 		for(String s:name)
 		{
 			System.out.println(s);
+			System.out.println("hi"+"hi");
 		}
 	}
 }
---
+++ A1/src/Vehicle.java
@@ -0,0 +1,48 @@
+
+public class Vehicle 
+{
+ /* public static void main(String[] args) {
+	
+	  
+	  int a=10;
+	  int b=6;
+	 
+	  divi(a,b);
+	  
+	  
+}
+
+private static void divi(int a, int b) {
+	// TODO Auto-generated method stub
+	
+	int q=0;
+	while(a>=b)
+	{
+		a=a-b;
+		q++;
+	}
+	
+	System.out.println(a);
+	System.out.println(q);
+}*/
+	
+	
+	public static void main(String[] args) {
+		int n=5;
+		int n1=0,n2=1,n3;
+		
+		
+		
+		for(int i=0;i<=n;i++)
+		{
+			System.out.println(n1);
+			n3=n1+n2;
+			n1=n2;
+			n2=n3;
+			
+			
+		}
+		
+	}
+	
+}
