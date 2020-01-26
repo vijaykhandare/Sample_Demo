84586119434abc5c28b655795b7a6e88cede4bc0
vijaykhandare <44311382+vijaykhandare@users.noreply.github.com>
2020-01-26 20:27:59 +0530

    something

--- A1/src/Demo.java
+++ A1/src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	
 	public static void main(String[] args) {
-		String name[]={"vijay","sanjay","ajay","billa","oggy"}; // this is string
+		String name[]={"vijay","sanjay","ajay","billa","oggyy"}; // this is string
 		for(String s:name)
 		{
 			System.out.println(s);
